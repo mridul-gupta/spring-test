34e287128571926ba2b45c46d3398238e51d5877
Mridul Gupta <mridul.gupta@reece.com.au>
2021-09-23 15:36:02 +1000

    again chapter 2.1

--- src/main/java/com/mg/learningspring/LearningSpringApplication.java
+++ src/main/java/com/mg/learningspring/LearningSpringApplication.java
@@ -1,11 +1,7 @@
 package com.mg.learningspring;
 
-import com.mg.learningspring.data.entity.Room;
-import com.mg.learningspring.data.repository.RoomRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,12 +15,12 @@ public class LearningSpringApplication {
     @RestController
     @RequestMapping("/rooms")
     public class RoomController {
-        @Autowired
-        private RoomRepository roomRepository;
-
-        @GetMapping
-        public Iterable<Room> getRooms() {
-            return this.roomRepository.findAll();
-        }
+//        @Autowired
+//        private RoomRepository roomRepository;
+//
+//        @GetMapping
+//        public Iterable<Room> getRooms() {
+//            return this.roomRepository.findAll();
+//        }
     }
 }
--- src/main/java/com/mg/learningspring/data/repository/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mg.learningspring.data.repository;
-
-import com.mg.learningspring.data.entity.Room;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoomRepository extends CrudRepository<Room, Long> {
-}
